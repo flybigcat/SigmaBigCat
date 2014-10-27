09cf730cca7590e355f0bcbc83fc2e294d3bfeba
Dell Black Giant <Dell Black Giant@DellBlackGiant>
2014-10-27 10:15:23 +0800

    sync with sigma's all codes

--- src/rbf/pfm.cc
+++ src/rbf/pfm.cc
@@ -19,37 +19,37 @@ PagedFileManager::PagedFileManager()
 
 PagedFileManager::~PagedFileManager()
 {
-    _pf_manager=NULL;
+	_pf_manager=NULL;
 }
 
 //This method creates a paged file called fileName. The file should not already exist.
 RC PagedFileManager::createFile(const char *fileName)
 {
-    FILE * pFile;
+	FILE * pFile;
 
-    //check if the file named "filName" exists(ready for input) as a binary file
-    pFile = fopen(fileName, "rb");
+	//check if the file named "filName" exists(ready for input) as a binary file
+	pFile = fopen(fileName, "rb");
 
-    if(pFile != NULL) //if already exist: close it and return error msg
-        {
-            fclose(pFile);
-            return -1;
-        }
+	if(pFile != NULL) //if already exist: close it and return error msg
+		{
+			fclose(pFile);
+			return -1;
+		}
 
-    //the file named "filName" ready for output as a binary file
-    pFile = fopen(fileName, "wb");
-    fclose(pFile);
+	//the file named "filName" ready for output as a binary file
+	pFile = fopen(fileName, "wb");
+	fclose(pFile);
 
-    //"fileName" pointer is created and "
+	//"fileName" pointer is created and "
     return 0;
 }
 
 //This method destroys the paged file whose name is fileName. The file should exist.
 RC PagedFileManager::destroyFile(const char *fileName)
 {
-    //check if a file named "fileName" exits
-//  if(fopen(fileName, "rb") == NULL) //not exits
-//      return -1;
+	//check if a file named "fileName" exits
+//	if(fopen(fileName, "rb") == NULL) //not exits
+//		return -1;
     ifstream f(fileName);
     if (!f.good()) {
           f.close();
@@ -57,11 +57,11 @@ RC PagedFileManager::destroyFile(const char *fileName)
       }else
           f.close();
 
-    //delete it and check
-    if( remove(fileName) != 0)
-        return -1; //delete failed
+	//delete it and check
+	if( remove(fileName) != 0)
+		return -1; //delete failed
 
-    return 0;
+	return 0;
 }
 
 /*This method opens the paged file whose name is fileName.
@@ -79,26 +79,26 @@ RC PagedFileManager::destroyFile(const char *fileName)
 
 RC PagedFileManager::openFile(const char *fileName, FileHandle &fileHandle)
 {
-    FILE * pFile;
+	FILE * pFile;
 
-    pFile = fopen(fileName, "rb");
+	pFile = fopen(fileName, "rb");
 
-    //check if a file named "fileName" exits
-    if(pFile == NULL) //not exits
-        return -1;
+	//check if a file named "fileName" exits
+	if(pFile == NULL) //not exits
+		return -1;
 
-    fclose(pFile);
+	fclose(pFile);
 
-    //check if the fileHandle is already a handle for an open file
-    if(fileHandle.hasFile())
-    {
-        return -1;
-    }
+	//check if the fileHandle is already a handle for an open file
+	if(fileHandle.hasFile())
+	{
+		return -1;
+	}
 
-    pFile = fopen(fileName, "r+b");
+	pFile = fopen(fileName, "r+b");
 
-    return fileHandle.loadFile(fileName, pFile);
-//  return 0;
+	return fileHandle.loadFile(fileName, pFile);
+//	return 0;
 
 }
 
@@ -108,11 +108,11 @@ RC PagedFileManager::openFile(const char *fileName, FileHandle &fileHandle)
  * */
 RC PagedFileManager::closeFile(FileHandle &fileHandle)
 {
-    //check if this fileHandle is a handle for an open file
-    if(!fileHandle.hasFile())
-        return -1;
+	//check if this fileHandle is a handle for an open file
+	if(!fileHandle.hasFile())
+		return -1;
 
-    fileHandle.closeFile();
+	fileHandle.closeFile();
 
     return 0;
 }
@@ -120,8 +120,8 @@ RC PagedFileManager::closeFile(FileHandle &fileHandle)
 
 FileHandle::FileHandle()
 {
-    //hFile = NULL;
-    isFile = false;
+	//hFile = NULL;
+	isFile = false;
 }
 
 
@@ -134,42 +134,42 @@ FileHandle::~FileHandle()
 //The page should exist. Note the page number starts from 0.
 RC FileHandle::readPage(PageNum pageNum, void *data)
 {
-    //cout<<getNumberOfPages();
+	//cout<<getNumberOfPages();
 
-    //check it this file has this page
-    if(pageNum > getNumberOfPages())
-        return -1;
+	//check it this file has this page
+	if(pageNum > getNumberOfPages())
+		return -1;
 
-    if(fseek(hFile, pageNum*PAGE_SIZE, SEEK_SET)!=0)
-        return -1;
+	if(fseek(hFile, pageNum*PAGE_SIZE, SEEK_SET)!=0)
+		return -1;
 
-    //Read block of data from stream
-    //size_t fread ( void * ptr, size_t size, size_t count, FILE * stream );
-    if(fread(data, PAGE_SIZE, 1, hFile) != 1)
-        return -1;
+	//Read block of data from stream
+	//size_t fread ( void * ptr, size_t size, size_t count, FILE * stream );
+	if(fread(data, PAGE_SIZE, 1, hFile) != 1)
+		return -1;
 
-    return 0;
+	return 0;
 }
 
 //This method writes the data into a page specified by the pageNum.
 //The page should exist. Note the page number starts from 0.
 RC FileHandle::writePage(PageNum pageNum, const void *data)
 {
-    //if the pageNum th is 1 more than the last page, add to the end
-    if(pageNum == getNumberOfPages())
-        appendPage(data);
+	//if the pageNum th is 1 more than the last page, add to the end
+	if(pageNum == getNumberOfPages())
+		appendPage(data);
 
-    //check if pageNum not exits OR can no be attached to the file
-    if(pageNum > getNumberOfPages())
-        return -1;
+	//check if pageNum not exits OR can no be attached to the file
+	if(pageNum > getNumberOfPages())
+		return -1;
 
-    if(fseek(hFile, pageNum*PAGE_SIZE, SEEK_SET)!=0)
-        return -1;
+	if(fseek(hFile, pageNum*PAGE_SIZE, SEEK_SET)!=0)
+		return -1;
 
-    //Write block of data to stream
-    //size_t fwrite ( const void * ptr, size_t size, size_t count, FILE * stream );
-    if (fwrite(data, PAGE_SIZE, 1, hFile) != 1)
-        return -1;
+	//Write block of data to stream
+	//size_t fwrite ( const void * ptr, size_t size, size_t count, FILE * stream );
+	if (fwrite(data, PAGE_SIZE, 1, hFile) != 1)
+		return -1;
 
     return 0;
 }
@@ -178,70 +178,70 @@ RC FileHandle::writePage(PageNum pageNum, const void *data)
 //and writes the data into the new allocated page.
 RC FileHandle::appendPage(const void *data)
 {
-    if(fseek(hFile, 0, SEEK_END)!=0)
-        return -1;
+	if(fseek(hFile, 0, SEEK_END)!=0)
+		return -1;
 
-    if(fwrite(data, PAGE_SIZE, 1, hFile) != 1)
-        return -1;
+	if(fwrite(data, PAGE_SIZE, 1, hFile) != 1)
+		return -1;
 
-    return 0;
+	return 0;
 
 }
 
 //This method returns the total number of pages in the file.
 unsigned FileHandle::getNumberOfPages()
 {
-    //Reposition stream position indicator
-    //int fseek ( FILE * stream, long int offset, int origin );
-    fseek(hFile, 0, SEEK_END);
+	//Reposition stream position indicator
+	//int fseek ( FILE * stream, long int offset, int origin );
+	fseek(hFile, 0, SEEK_END);
 
-    //Get current position in stream
-    //long int ftell ( FILE * stream );
-    numberOfPages =  (ftell(hFile))/PAGE_SIZE;
+	//Get current position in stream
+	//long int ftell ( FILE * stream );
+	numberOfPages =  (ftell(hFile))/PAGE_SIZE;
 
-    return numberOfPages;
+	return numberOfPages;
 
-    return -1;
+	return -1;
 }
 
 RC FileHandle::loadFile(const char * fileName, FILE * loadedFile)
 {
-    hFileName.assign(fileName);
-    hFile = loadedFile;
-    isFile = true;
-    return 0;
+	hFileName.assign(fileName);
+	hFile = loadedFile;
+	isFile = true;
+	return 0;
 }
 
 RC FileHandle::unloadFile()
 {
-    hFile = NULL;
-    isFile = false;
-    return 0;
+	hFile = NULL;
+	isFile = false;
+	return 0;
 }
 
 RC FileHandle::closeFile()
 {
-    if(!isFile)
-        return -1;
+	if(!isFile)
+		return -1;
 
-    // All of the file's pages are flushed to disk
-    fflush(hFile);
+	// All of the file's pages are flushed to disk
+	fflush(hFile);
 
-    fclose(hFile);
-    return 0;
+	fclose(hFile);
+	return 0;
 }
 
 bool FileHandle::hasFile()
 {
-    return isFile;
+	return isFile;
 }
 
 FILE FileHandle::getFile()
 {
-    return  *hFile;
+	return  *hFile;
 }
 
 string FileHandle::getFileName()
 {
-    return hFileName;
+	return hFileName;
 }
